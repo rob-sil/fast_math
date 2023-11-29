6f72c46e10951f96d93f37215666502e29e08676
Rob <124158982+rob-sil@users.noreply.github.com>
2023-11-29 08:15:29 -0600

    Move dimension checks to Rust

--- python/fast_math/sum.py
+++ python/fast_math/sum.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from .rust_fast_math import sum_32_1d, sum_32_2d, sum_32_dyn
+from .rust_fast_math import sum_32
 
 
 def sum(array: np.ndarray) -> np.floating:
@@ -11,9 +11,4 @@ def sum(array: np.ndarray) -> np.floating:
     if array.dtype != np.float32:
         raise NotImplementedError("Only float32s are currently supported.")
 
-    if array.ndim == 1:
-        return np.float32(sum_32_1d(array))
-    elif array.ndim == 2:
-        return np.float32(sum_32_2d(array))
-    else:
-        return np.float32(sum_32_dyn(array))
+    return np.float32(sum_32(array))
--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,32 +1,32 @@
-use pyo3::prelude::*;
+use pyo3::{exceptions::PyValueError, prelude::*};
 
 use numpy::{PyReadonlyArray1, PyReadonlyArray2, PyReadonlyArrayDyn};
 
 mod expansion;
 mod sum;
 
-use sum::online_sum;
-
 #[pyfunction]
-fn sum_32_1d(x: PyReadonlyArray1<f32>) -> PyResult<f32> {
-    Ok(online_sum::<_, 7>(x.as_array()))
-}
+fn sum_32(array: &PyAny) -> PyResult<f32> {
+    let ndims = match PyReadonlyArrayDyn::<f32>::extract(array) {
+        Ok(a) => a.shape().len(),
+        Err(_) => return Err(PyValueError::new_err("Only NumPy arrays are supported.")),
+    };
 
-#[pyfunction]
-fn sum_32_2d(x: PyReadonlyArray2<f32>) -> PyResult<f32> {
-    Ok(online_sum::<_, 7>(x.as_array()))
-}
-
-#[pyfunction]
-// Significantly slower due to runtime-determined dimension
-fn sum_32_dyn(x: PyReadonlyArrayDyn<f32>) -> PyResult<f32> {
-    Ok(online_sum::<_, 7>(x.as_array()))
+    match ndims {
+        1 => Ok(sum::online_sum::<_, 7>(
+            PyReadonlyArray1::extract(array)?.as_array(),
+        )),
+        2 => Ok(sum::online_sum::<_, 7>(
+            PyReadonlyArray2::extract(array)?.as_array(),
+        )),
+        _ => Ok(sum::online_sum::<_, 7>(
+            PyReadonlyArrayDyn::extract(array)?.as_array(),
+        )),
+    }
 }
 
 #[pymodule]
 fn rust_fast_math(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_32_1d, m)?)?;
-    m.add_function(wrap_pyfunction!(sum_32_2d, m)?)?;
-    m.add_function(wrap_pyfunction!(sum_32_dyn, m)?)?;
+    m.add_function(wrap_pyfunction!(sum_32, m)?)?;
     Ok(())
 }
