a53305345836191b095343d680f9328b46a78023
Rob <124158982+rob-sil@users.noreply.github.com>
2023-12-06 16:13:13 -0600

    Improve the benchmark code

--- benchmark/benchmark.py
+++ benchmark/benchmark.py
@@ -1,54 +1,132 @@
+from collections import defaultdict
+from dataclasses import dataclass, field
 from timeit import timeit
-from typing import List, Tuple
+from typing import Dict, List
 
 import numpy as np
 
 import fast_math as fm
 
-benchmarks: List[Tuple[str, str]] = [
-    (
-        "np.ones({N}, dtype=np.float32)",
-        "sum(array)",
+
+@dataclass
+class Benchmark:
+    setup: str
+    """Format string to create an array of size `N`."""
+    statement: str
+    """Statement to run, taking in the array created by `setup`."""
+    tags: Dict[str, str] = field(default_factory=dict)
+    """Optional tags to print result breakdowns."""
+
+    def timeit(self, N: int, sum, num_runs: int = 100) -> float:
+        """Time the benchmark for sum function `sum` on an array of roughly
+        `N` elements.
+        """
+        return (
+            timeit(
+                self.statement,
+                "array = " + self.setup.format(N=N),
+                number=num_runs,
+                globals={"sum": sum, "np": np},
+            )
+            / num_runs
+        )
+
+
+benchmarks: List[Benchmark] = [
+    Benchmark(
+        setup="np.ones({N}, dtype=np.float32)",
+        statement="sum(array)",
+        tags={"Dimensions": "1", "Axis (`axis`)": "Full Array"},
     ),
-    (
-        "np.arange({N}, dtype=np.float32)",
-        "sum(array)",
+    Benchmark(
+        setup="np.arange({N}, dtype=np.float32)",
+        statement="sum(array)",
+        tags={"Dimensions": "1", "Axis (`axis`)": "Full Array"},
     ),
-    (
-        "np.arange({N}, dtype=np.float32)[::-1]",
-        "sum(array)",
+    Benchmark(
+        setup="np.arange({N}, dtype=np.float32)[::-1]",
+        statement="sum(array)",
+        tags={"Dimensions": "1", "Axis (`axis`)": "Full Array"},
     ),
-    (
-        "np.array([2**24, 1, -(2**24)] * ({N} // 3), dtype=np.float32)",
-        "sum(array)",
+    Benchmark(
+        setup="np.array([2**24, 1, -(2**24)] * ({N} // 3), dtype=np.float32)",
+        statement="sum(array)",
+        tags={"Dimensions": "1", "Axis (`axis`)": "Full Array"},
     ),
-    (
-        "np.ones({N} // 5 * 5, dtype=np.float32).reshape((-1, 5))",
-        "sum(array)",
+    Benchmark(
+        setup="np.arange({N}, dtype=np.float32)[::-1]",
+        statement="sum(array)",
+        tags={"Dimensions": "1", "Axis (`axis`)": "Full Array"},
     ),
-    (
-        "np.arange({N} // 5 * 5, dtype=np.float32).reshape((-1, 5))",
-        "sum(array)",
+    Benchmark(
+        setup="np.ones({N} // 5 * 5, dtype=np.float32).reshape((-1, 5))",
+        statement="sum(array)",
+        tags={"Dimensions": "2", "Axis (`axis`)": "Full Array"},
     ),
-    (
-        "np.ones({N} // 25 * 25, dtype=np.float32).reshape((-1, 5, 5))",
-        "sum(array)",
+    Benchmark(
+        setup="np.arange({N} // 5 * 5, dtype=np.float32).reshape((-1, 5))",
+        statement="sum(array)",
+        tags={"Dimensions": "2", "Axis (`axis`)": "Full Array"},
     ),
-    (
-        "np.arange({N} // 25 * 25, dtype=np.float32).reshape((-1, 5, 5))",
-        "sum(array)",
+    Benchmark(
+        setup="np.ones({N} // 25 * 25, dtype=np.float32).reshape((-1, 5, 5))",
+        statement="sum(array)",
+        tags={"Dimensions": "3", "Axis (`axis`)": "Full Array"},
     ),
-    (
-        "np.ones({N} // 5 * 5, dtype=np.float32).reshape((5, -1))",
-        "sum(array, axis=0)",
+    Benchmark(
+        setup="np.arange({N} // 25 * 25, dtype=np.float32).reshape((-1, 5, 5))",
+        statement="sum(array)",
+        tags={"Dimensions": "3", "Axis (`axis`)": "Full Array"},
     ),
-    (
-        "np.arange({N} // 5 * 5, dtype=np.float32).reshape((5, -1))",
-        "sum(array, axis=0)",
+    Benchmark(
+        setup="np.ones({N} // 5 * 5, dtype=np.float32).reshape((5, -1))",
+        statement="sum(array)",
+        tags={"Dimensions": "2", "Axis (`axis`)": "Full Array"},
     ),
-    (
-        "np.arange({N} // 25 * 25, dtype=np.float32).reshape((5, 5, -1))",
-        "sum(array, axis=0)",
+    Benchmark(
+        setup="np.arange({N} // 5 * 5, dtype=np.float32).reshape((5, -1))",
+        statement="sum(array)",
+        tags={"Dimensions": "2", "Axis (`axis`)": "Full Array"},
+    ),
+    Benchmark(
+        setup="np.ones({N} // 25 * 25, dtype=np.float32).reshape((5, 5, -1))",
+        statement="sum(array)",
+        tags={"Dimensions": "3", "Axis (`axis`)": "Full Array"},
+    ),
+    Benchmark(
+        setup="np.arange({N} // 25 * 25, dtype=np.float32).reshape((5, 5, -1))",
+        statement="sum(array)",
+        tags={"Dimensions": "3", "Axis (`axis`)": "Full Array"},
+    ),
+    Benchmark(
+        setup="np.ones({N} // 5 * 5, dtype=np.float32).reshape((5, -1))",
+        statement="sum(array, axis=0)",
+        tags={"Dimensions": "2", "Axis (`axis`)": "By Axis (Short)"},
+    ),
+    Benchmark(
+        setup="np.arange({N} // 5 * 5, dtype=np.float32).reshape((5, -1))",
+        statement="sum(array, axis=0)",
+        tags={"Dimensions": "2", "Axis (`axis`)": "By Axis (Short)"},
+    ),
+    Benchmark(
+        setup="np.arange({N} // 25 * 25, dtype=np.float32).reshape((5, 5, -1))",
+        statement="sum(array, axis=0)",
+        tags={"Dimensions": "3", "Axis (`axis`)": "By Axis (Short)"},
+    ),
+    Benchmark(
+        setup="np.ones({N} // 5 * 5, dtype=np.float32).reshape((-1, 5))",
+        statement="sum(array, axis=0)",
+        tags={"Dimensions": "2", "Axis (`axis`)": "By Axis (Long)"},
+    ),
+    Benchmark(
+        setup="np.arange({N} // 5 * 5, dtype=np.float32).reshape((-1, 5))",
+        statement="sum(array, axis=0)",
+        tags={"Dimensions": "2", "Axis (`axis`)": "By Axis (Long)"},
+    ),
+    Benchmark(
+        setup="np.arange({N} // 25 * 25, dtype=np.float32).reshape((-1, 5, 5))",
+        statement="sum(array, axis=0)",
+        tags={"Dimensions": "3", "Axis (`axis`)": "By Axis (Long)"},
     ),
 ]
 
@@ -69,37 +147,28 @@ def time_format(seconds) -> str:
 
 if __name__ == "__main__":
     results = []
+    seen_tags = defaultdict(set)
 
-    num_runs = 100
-    for setup_format, statement in benchmarks:
+    for benchmark in benchmarks:
         for N in sizes:
-            setup = "array = " + setup_format.format(N=N)
+            print(f"> array = {benchmark.setup.format(N=N)}\n> {benchmark.statement}")
 
-            print(f"> {setup}\n> {statement}")
-
-            fast_time = (
-                timeit(
-                    statement, setup, number=num_runs, globals={"sum": fm.sum, "np": np}
-                )
-                / num_runs
-            )
+            fast_time = benchmark.timeit(N, fm.sum)
             print(f"\tfast_math: {time_format(fast_time)}")
 
-            numpy_time = (
-                timeit(
-                    statement, setup, number=num_runs, globals={"sum": np.sum, "np": np}
-                )
-                / num_runs
-            )
+            numpy_time = benchmark.timeit(N, np.sum)
             print(f"\t    numpy: {time_format(numpy_time)}")
 
-            results.append(
-                {
-                    "N": N,
-                    "fast_math": fast_time,
-                    "numpy": numpy_time,
-                }
-            )
+            result = {
+                "N": N,
+                "fast_math": fast_time,
+                "numpy": numpy_time,
+            }
+            for tag_name, tag_value in benchmark.tags.items():
+                result[tag_name] = tag_value
+                seen_tags[tag_name].add(tag_value)
+
+            results.append(result)
 
             print()
 
@@ -119,6 +188,33 @@ if __name__ == "__main__":
             f"| {N:,} | {time_format(fast_time)} | {time_format(numpy_time)} "
             f"| {fast_time / numpy_time:.1f}x |\n"
         )
+
+    for tag_name, tag_values in seen_tags.items():
+        markdown += "\n## Results: " + tag_name + "\n"
+
+        markdown += f"| {tag_name} | `fast_math` | NumPy | Slowdown |\n"
+        markdown += "| -: | -: | -: | -: |\n"
+
+        for tag_value in sorted(tag_values):
+            fast_time = np.mean(
+                [
+                    result["fast_math"]
+                    for result in results
+                    if result.get(tag_name, None) == tag_value
+                ]
+            )
+            numpy_time = np.mean(
+                [
+                    result["numpy"]
+                    for result in results
+                    if result.get(tag_name, None) == tag_value
+                ]
+            )
+
+            markdown += (
+                f"| {tag_value} | {time_format(fast_time)} | {time_format(numpy_time)} "
+                f"| {fast_time / numpy_time:.1f}x |\n"
+            )
 
     with open("benchmark.md", "w") as f:
         f.write(markdown)
